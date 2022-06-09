3c7363a7c2bc2fcae7892470ab5c83516e564f2c
Ahmad Aulia Wiguna <aulia@softwareseni.com>
2022-06-09 15:53:47 +0700

    feat(fe):Use pagination

--- frontend/components/table.js
+++ frontend/components/table.js
@@ -1,6 +1,8 @@
+import Link from 'next/link'
 import React, { useMemo, useState, useEffect } from 'react'
 import { useTable, usePagination, useSortBy } from 'react-table'
-import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
+import { TriangleDownIcon, TriangleUpIcon, ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
+import { Button, ButtonGroup, Stack } from '@chakra-ui/react'
 import _ from 'lodash'
 import {
     Table,
@@ -21,15 +23,18 @@ export default function TableUI({ columns, data, url }) {
     const { data: session, status } = useSession()
     const isUser = !!session?.user
     const [records, setRecord] = useState([])
+    const [totalPages, setTotalPages] = useState(1)
+    const [totalRecords, setTotalRecords] = useState(1)
     const [page, setPage] = useState(1)
     const [per_page, setPerPage] = useState(5)
   
     React.useEffect(() => {
-        fetchData(session)
+        fetchData(session, 1, 10)
     }, [isUser, status])
 
-    const fetchData = async (session) => {
+    const fetchData = async (session, page, per_page) => {
         if (!_.isNil(session)) {
+            setPage(page)
             let apiUrl = url
             axios.defaults.headers.common['Authorization'] = `Bearer ${session.accessToken}`
             let records = await axios.get(apiUrl, {
@@ -38,13 +43,63 @@ export default function TableUI({ columns, data, url }) {
                     per_page: per_page    
                 }
             }).then((response) => {
-                return response.data.data.rows
+                return response.data.data
             }).catch((error) => {
                 return error.message
             })    
 
-            setRecord(records)                
+            if (!_.isNil(records.rows)) {
+                setRecord(records.rows)                
+            }
+            if (!_.isNil(records.pages)) {
+                setTotalPages(records.pages)                
+            }
+            if (!_.isNil(records.count)) {
+                setTotalRecords(records.pages)                
+            }
         }
+    }
+
+    const PaginationPanel = () => {
+        const items = []
+        if (totalPages>1) {
+            if (page>1) {
+                items.push(<Button onClick={() => {fetchData(session, page-1, 10)}}  leftIcon={<ChevronLeftIcon />} colorScheme='facebook' variant='outline'>Previous Page</Button>)
+            }
+            if (totalPages<=10) {
+                for (let index = 1; index <= totalPages; index++) {
+                    items.push(<Button key={index} onClick={() => {fetchData(session, index, 10)}} isActive={page==index} colorScheme='facebook' variant='outline'>{index}</Button>)
+                }
+            } else {
+                if (page<=4) {
+                    for (let index = 1; index <= 5; index++) {
+                        items.push(<Button key={index} onClick={() => {fetchData(session, index, 10)}} isActive={page==index} colorScheme='facebook' variant='outline'>{index}</Button>)
+                    }                
+                    items.push(<Button key={'separator1'} colorScheme='facebook' variant='outline'>{'...'}</Button>)
+                    items.push(<Button key={totalPages} onClick={() => {fetchData(session, totalPages, 10)}} isActive={page==totalPages} colorScheme='facebook' variant='outline'>{totalPages}</Button>)
+                } else if (page>4 && page<totalPages-4) {
+                    items.push(<Button key={1} onClick={() => {fetchData(session, 1, 10)}} isActive={page==1} colorScheme='facebook' variant='outline'>{'1'}</Button>)
+                    items.push(<Button key={'separator1'} colorScheme='facebook' variant='outline'>{'...'}</Button>)
+                    items.push(<Button mr={5} onClick={() => {fetchData(session, page-1, 10)}} colorScheme='facebook' variant='outline'>{page-1}</Button>)
+                    items.push(<Button mr={5} onClick={() => {fetchData(session, page, 10)}} isActive colorScheme='facebook' variant='outline'>{page}</Button>)
+                    items.push(<Button mr={5} onClick={() => {fetchData(session, page+1, 10)}} colorScheme='facebook' variant='outline'>{page+1}</Button>)
+                    items.push(<Button key={'separator2'} colorScheme='facebook' variant='outline'>{'...'}</Button>)
+                    items.push(<Button key={totalPages} onClick={() => {fetchData(session, totalPages, 10)}} isActive={page==totalPages} colorScheme='facebook' variant='outline'>{totalPages}</Button>)
+                } else {
+                    items.push(<Button key={1} onClick={() => {fetchData(session, 1, 10)}} isActive={page==1} colorScheme='facebook' variant='outline'>{'1'}</Button>)
+                    items.push(<Button key={'separator2'} colorScheme='facebook' variant='outline'>{'...'}</Button>)
+                    for (let index = totalPages-4; index <= totalPages; index++) {
+                        items.push(<Button key={index} onClick={() => {fetchData(session, index, 10)}} isActive={page==index} colorScheme='facebook' variant='outline'>{index}</Button>)
+                    }
+                }
+            }
+            if (page<totalPages) {
+                items.push(<Button onClick={() => {fetchData(session, page+1, 10)}}  rightIcon={<ChevronRightIcon />} colorScheme='facebook' variant='outline'>Next Page</Button>)
+            }
+
+            return items
+        }
+        return (<></>)
     }
 
 
@@ -109,6 +164,9 @@ export default function TableUI({ columns, data, url }) {
                         }
                     </Tbody>
                 </Table>
+                <Stack mb={16} spacing={2} direction="row" align='center'>
+                    <PaginationPanel />
+                </Stack>
             </TableContainer>
         </>
     )
