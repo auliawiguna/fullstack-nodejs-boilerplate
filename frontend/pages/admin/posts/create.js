e0dcb420db17a8bd99d1ea223e162eb0a8afd1a9
Ahmad Aulia Wiguna <aulia@softwareseni.com>
2022-07-17 23:45:54 +0700

    wip(fe):create form

---
+++ frontend/pages/admin/posts/create.js
@@ -0,0 +1,162 @@
+import { DashboardLayout } from '@layouts/dashboard'
+import Head from 'next/head'
+import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/router'
+import { FaSave, FaArrowLeft } from 'react-icons/fa'
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    Box,
+    Stack,
+    FormControl,
+    Input,
+    Button,
+    FormLabel,
+    Text,
+} from '@chakra-ui/react'
+import { useState } from 'react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { getSession } from 'next-auth/react'
+import axios from 'axios'
+import { useToast } from '@chakra-ui/react'
+import { backDialog } from '@utils/swal'
+import { create } from '@validations/post'
+
+const CreatePage = (props) => {
+    // const { data: session, status } = useSession()
+    const session = props.sessions
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    const router = useRouter()
+
+    const validationSchema = create
+
+    const toast = useToast()
+
+    const formOptions = { resolver: yupResolver(validationSchema) }
+
+    // get functions to build form with useForm() hook
+    const { register, handleSubmit, setError, formState } = useForm(formOptions)
+
+    const { errors } = formState    
+    
+    const back = () => { return backDialog(router, "/admin/posts") }
+
+    const submit = async (values) => {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${session.accessToken}`
+        let url = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/posts`
+        try {
+            setIsLoading(true)            
+            await axios.post(url, values).then((response) => {
+                if (!response.error) {
+                    toast({
+                        position: 'top-right',
+                        title: 'Success.',
+                        description: "Record Created.",
+                        status: 'success',
+                        duration: 9000,
+                        isClosable: true,
+                    })                
+                    setIsLoading(false)
+                    return router.push("/admin/posts")
+                }            
+            })            
+        } catch (error) {
+            setIsLoading(false)
+            toast({
+                position: 'top-right',
+                title: 'Error.',
+                description: error.response.data.message,
+                status: 'error',
+                duration: 9000,
+                isClosable: true,
+            })                            
+        }
+    }
+    
+    return (
+        <>
+            <Head>
+                <title>Create Post</title>
+            </Head>
+            <DashboardLayout title="Create Post">
+                <Breadcrumb mb={4} fontWeight='medium' fontSize='sm'>
+                    <BreadcrumbItem>
+                        <Link scroll={false} href='/admin/dashboard'>Home</Link>
+                    </BreadcrumbItem>
+
+                    <BreadcrumbItem>
+                        <Link scroll={false} href='/admin/posts'>Posts</Link>
+                    </BreadcrumbItem>
+
+                    <BreadcrumbItem isCurrentPage>
+                        <Link  scroll={false} href='#'>Create Posts</Link>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+                <div>
+                    <Box w={{sm: "full", md:'50%', lg:'50%' }}>
+                        <form onSubmit={handleSubmit(submit)}>
+                            <Stack
+                            spacing={4}
+                            p="1rem"
+                            >
+                                <FormControl isRequired>
+                                    <FormLabel htmlFor='title'>Title</FormLabel>
+                                    <Input
+                                        id='title'
+                                        type='text'
+                                        {...register('title')}
+                                    />
+                                    <Text color={'red'} className="invalid-feedback">{errors.title  ?.message}</Text>
+                                </FormControl>
+                                <Stack direction='row' spacing={4}>
+                                    <Button
+                                        leftIcon={<FaArrowLeft />}
+                                        isLoading={isLoading}
+                                        borderRadius={0}
+                                        type="button"
+                                        onClick={() => {back()}}
+                                        variant="outline"
+                                        colorScheme="orange"
+                                        width="50%"
+                                    >
+                                        Back
+                                    </Button>
+
+                                    <Button
+                                        leftIcon={<FaSave />}
+                                        isLoading={isLoading}
+                                        loadingText='Submitting'                            
+                                        borderRadius={0}
+                                        type="submit"
+                                        variant="outline"
+                                        colorScheme="teal"
+                                        width="50%"
+                                    >
+                                        Submit
+                                    </Button>
+                                </Stack>                            
+                            </Stack>
+                        </form>
+
+                    </Box>
+                </div>
+            </DashboardLayout>
+        </>
+    )
+}
+
+CreatePage.auth = true 
+
+export async function getServerSideProps(context) {
+    let session = await getSession(context)
+    return {
+        props: {
+            sessions: session,
+        }
+    }
+}
+
+export default CreatePage
